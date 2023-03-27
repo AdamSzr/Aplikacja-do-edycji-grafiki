65be5837be8f8a25e5fdd88ce8012502e211bb9f
Adam Szreiber <adam.szr98@gmail.com>
2023-03-27 20:33:49 +0200

    Correctly drawing on board

--- src/features/ImageEditor/Tools/DrawOnBoard.tsx
+++ src/features/ImageEditor/Tools/DrawOnBoard.tsx
@@ -6,42 +6,43 @@ type Point = { x: number, y: number }
 
 const DrawOnBoard = () => {
   const ctx = useContext(ImageEditorContext)
+  const [isDrawing, setIsDrawing] = useState(false)
+
   const canvasRef = useRef<HTMLCanvasElement>()
-  const [canvasCtx, setCanvasCtx] = useState<CanvasRenderingContext2D>()
-  const [lastPos, setLastPos] = useState<null | Point>()
+  const canvasCtxRef = useRef<CanvasRenderingContext2D>()
+  const lastPointPos = useRef<Point>()
 
-  function showCords(e: MouseEvent) {
-    const pos: Point = { x: e.clientX, y: e.clientY }
-    // console.log(canvasCtx, e, pos)
-    // if (!canvasCtx) return
+  function showCords({ offsetX, offsetY }: MouseEvent) {
+    if (!isDrawing || !canvasCtxRef.current)
+      return
 
-    console.log(lastPos, pos)
+    const point: Point = { x: offsetX, y: offsetY }
 
-    if (!lastPos) {
-      setLastPos(pos)
+
+    if (lastPointPos.current == undefined) {
+      lastPointPos.current = point
       return
     }
 
+    const prevPoint = lastPointPos.current
 
-    if (lastPos) {
-      canvasCtx!.beginPath();
-      canvasCtx!.moveTo(lastPos.x, lastPos.y);
-      canvasCtx!.lineTo(pos.x, pos.y);
-      canvasCtx!.stroke();
-      setLastPos(pos)
-    } 
+
+
+    canvasCtxRef.current.beginPath()
+    canvasCtxRef.current.moveTo(prevPoint.x, prevPoint.y);
+    canvasCtxRef.current.lineTo(point.x, point.y);
+    canvasCtxRef.current.stroke();
+
+    lastPointPos.current = point
   }
+
+
+
 
   useEffect(() => {
     if (canvasRef.current) {
-      const board = canvasRef.current
       const ctx2D = canvasRef.current!.getContext(`2d`)!
-
-      setCanvasCtx(ctx2D)
-      board.addEventListener("mousemove", (e) => showCords(e));
-      board.addEventListener("mousedown", (e) => showCords(e));
-      board.addEventListener("mouseup", (e) => showCords(e));
-      board.addEventListener("mouseout", (e) => showCords(e));
+      canvasCtxRef.current = ctx2D
     } else {
       console.log("WAIT - canvas not loaded")
     }
@@ -51,7 +52,15 @@ const DrawOnBoard = () => {
   return (
     <div>
       <button onClick={() => { console.log('trzeba zaladowac plik') }} > zaladuj plik z pamieci </button>
-      <canvas ref={canvasRef as any} width={500} height={500} style={{ border: '1px solid black' }}>
+      <canvas
+        onMouseDown={e => setIsDrawing(true)}
+        onMouseMove={e => showCords(e.nativeEvent)}
+        onMouseUp={e => {
+          console.log("Mouse up")
+          setIsDrawing(false);
+          lastPointPos.current = undefined
+        }}
+        ref={canvasRef as any} width={500} height={500} style={{ border: '1px solid black' }}>
 
       </canvas>
 
