f959ae6e694ee5d19b0a10613a9b32b11efb4f0e
Adam Szreiber <adam.szr98@gmail.com>
2023-03-29 00:11:09 +0200

    removed basic canvas display from diff component

--- src/features/ImageEditor/Tools/DiffComponent.tsx
+++ src/features/ImageEditor/Tools/DiffComponent.tsx
@@ -11,29 +11,41 @@ export const DiffComponent = () => {
     useEffect(() => {
         const promiseTable = [imageCompression.drawFileInCanvas(ctx.originalFile!), imageCompression.drawFileInCanvas(ctx.processedFile!)]
 
+        ctx.canvas.current!.style.display = "none"
+
+        console.log("hide")
         Promise.all(promiseTable).then((e) => {
             const orginalImg = e.at(0)
             const processedImg = e.at(1)
 
-            const orgImgBitmap:ImageBitmap= (orginalImg!.at(0) as ImageBitmap)
-            const procImgBitmap:ImageBitmap= (processedImg!.at(0) as ImageBitmap)
+            const orgImgBitmap: ImageBitmap = (orginalImg!.at(0) as ImageBitmap)
+            const procImgBitmap: ImageBitmap = (processedImg!.at(0) as ImageBitmap)
 
             orginalRef.current?.getContext(`2d`)?.drawImage(orgImgBitmap, 0, 0)
             processedRef.current?.getContext(`2d`)?.drawImage(procImgBitmap, 0, 0)
             console.log({ processedImg, pp: orginalImg!.at(0), orginalImg, oi: orginalImg!.at(0), e })
-            // setElement(<>
-            // {(orginalImg?.[1] as unknown as OffscreenCanvas).getContext('2d')}
-            // {(processedImg?.[1] as unknown as OffscreenCanvas).getContext('2d')}
-            // </>)
+
         })
+        return () => {
+            console.log("unset")
+            ctx.canvas.current!.style.display = "block"
+        }
+
 
     }, [])
 
 
     return (
         <div>
-            <canvas ref={orginalRef as any} width={ctx.canvasSize?.w} height={ctx.canvasSize?.h} />
-            <canvas ref={processedRef as any} width={ctx.canvasSize?.w} height={ctx.canvasSize?.h} />
+            <div>
+                <h2> Oryginalny obraz</h2>
+                <canvas ref={orginalRef as any} width={ctx.canvasSize?.w} height={ctx.canvasSize?.h} />
+            </div>
+            <div>
+
+                <h2 > Skompresowany o obraz</h2>
+                <canvas ref={processedRef as any} width={ctx.canvasSize?.w} height={ctx.canvasSize?.h} />
+            </div>
         </div>
     )
 }
