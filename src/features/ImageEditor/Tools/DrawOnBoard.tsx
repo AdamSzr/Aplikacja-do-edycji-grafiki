10cd95ebeee9ce458ddf61eb8dad2ebcab2487f5
Adam Szreiber <adam.szr98@gmail.com>
2023-04-30 12:43:27 +0200

    Rectangle draw

--- src/features/ImageEditor/Tools/DrawOnBoard.tsx
+++ src/features/ImageEditor/Tools/DrawOnBoard.tsx
@@ -1,11 +1,12 @@
 
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useMemo, useRef, useState } from 'react'
 import BlackboardImage from '../BlackboardImage'
 import { ImageEditorContext } from '../ImageEditor'
 import imageCompression from 'browser-image-compression'
 import { IconButton } from '@mui/material'
 import CreateIcon from '@mui/icons-material/Create';
 import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
+import Brightness1Icon from '@mui/icons-material/Brightness1';
 
 type Point = { x: number, y: number }
 export type DrawTool = "pen" | "circle" | "rectangle"
@@ -18,16 +19,22 @@ const DrawOnBoard = () => {
   const isDrawing = useRef<boolean>(false)
   const [drawTool, setDrawTool] = useState<DrawTool | null>(null)
 
-  const [originalFile, setOriginalFile] = useState<File | null>(null)
-
-
-  useEffect(() => {
+  const originalFile = useMemo(() => {
     if (ctx.originalFile) {
-      const fileCpy = new File([ctx.originalFile!], 'copy.jpg')
-      setOriginalFile(fileCpy)
-      console.log("made cpy")
+      return new File([ctx.originalFile!], 'copy.jpg')
     }
   }, [])
+
+  // [originalFile, setOriginalFile] = useState<File | null>(null)
+
+
+  // useEffect(() => {
+  //   if (ctx.originalFile && !originalFile) {
+  //     const fileCpy = new File([ctx.originalFile!], 'copy.jpg')
+  //     setOriginalFile(fileCpy)
+  //     console.log("made cpy")
+  //   }
+  // }, [])
 
   const cleanCanvas = () => {
     console.log(ctx)
@@ -52,6 +59,38 @@ const DrawOnBoard = () => {
   }
 
 
+
+  const drawCircleEffect = () => {
+    const onMouseUp = ({ offsetX, offsetY }: MouseEvent) => {
+      if (!lastPointPos.current) return
+
+      isDrawing.current = (false);
+      const ctx = canvasContext.current!
+      const { x, y } = lastPointPos.current
+
+      console.log('up', { offsetX, offsetY })
+      ctx.beginPath();
+      ctx.lineWidth = Number("6");
+      ctx.strokeStyle = "red";
+      ctx.rect(x, y, offsetX - x, offsetY - y);
+      ctx.stroke();
+
+      lastPointPos.current = undefined
+    }
+    const onMouseDown = ({ offsetX, offsetY }: MouseEvent) => {
+      lastPointPos.current = { x: offsetX, y: offsetY }
+      console.log('down', { offsetX, offsetY })
+      isDrawing.current = (true);
+    }
+    canvas.current?.addEventListener('mouseup', onMouseUp)
+    canvas.current?.addEventListener('mousedown', onMouseDown)
+
+    return () => {
+      canvas.current?.removeEventListener('mouseup', onMouseUp)
+      canvas.current?.removeEventListener('mousedown', onMouseDown)
+    }
+  }
+
   const drawLineEffect = () => {
     const onMuseMove = (it: MouseEvent) => { drawLine(it) }
     const onMouseUp = () => { isDrawing.current = (false); lastPointPos.current = undefined }
@@ -59,12 +98,11 @@ const DrawOnBoard = () => {
     canvas.current?.addEventListener('mousemove', onMuseMove)
     canvas.current?.addEventListener('mouseup', onMouseUp)
     canvas.current?.addEventListener('mousedown', onMouseDown)
-    return () => {
 
+    return () => {
       canvas.current?.removeEventListener('mousemove', onMuseMove)
       canvas.current?.removeEventListener('mouseup', onMouseUp)
       canvas.current?.removeEventListener('mousedown', onMouseDown)
-
     }
   }
 
@@ -75,26 +113,16 @@ const DrawOnBoard = () => {
       ctx.setOriginalFile(file)
       ctx.setActiveFile('original')
     })
-    // URL.createObjectURL(await imageCompression.canvasToFile(canvas.current))
 
     if (drawTool == 'pen') {
       return drawLineEffect()
     }
+    if (drawTool == 'circle') {
+      console.log('drawingCircle')
+      return drawCircleEffect()
+    }
 
-    // const onMuseMove = (it: MouseEvent) => { drawLine(it) }
-    // const onMouseUp = () => { isDrawing.current = (false); lastPointPos.current = undefined }
-    // const onMouseDown = () => { isDrawing.current = (true) }
-    // canvas.current?.addEventListener('mousemove', onMuseMove)
-    // canvas.current?.addEventListener('mouseup', onMouseUp)
-    // canvas.current?.addEventListener('mousedown', onMouseDown)
-    // return () => {
-
-    //   canvas.current?.removeEventListener('mousemove', onMuseMove)
-    //   canvas.current?.removeEventListener('mouseup', onMouseUp)
-    //   canvas.current?.removeEventListener('mousedown', onMouseDown)
-    // }
-    
-  }, [])
+  }, [drawTool])
 
 
   const setToolPen = () => {
@@ -102,10 +130,17 @@ const DrawOnBoard = () => {
     console.log(`setting - pen`)
   }
 
+  const setToolCircle = () => {
+    setDrawTool('circle')
+    console.log(`setting - circle`)
+  }
+
   useEffect(() => {
     ctx.setToolboxItems(<>
       <IconButton onClick={cleanCanvas}> <CleaningServicesIcon fontSize="inherit" /></IconButton>
-      <IconButton onClick={setToolPen} ><CreateIcon fontSize="inherit" /> </IconButton>
+      <IconButton onClick={setToolPen} > <CreateIcon fontSize="inherit" /> </IconButton>
+      <IconButton onClick={setToolCircle} > <Brightness1Icon /> </IconButton>
+
     </>)
   }, [])
 
