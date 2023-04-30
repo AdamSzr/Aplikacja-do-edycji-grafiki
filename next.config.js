6cf5a5b23c2168791533326353665ab76e3e8792
Adam Szreiber <adam.szr98@gmail.com>
2023-04-30 09:08:03 +0200

    redirecting from / to /image-editor

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/image-editor',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
